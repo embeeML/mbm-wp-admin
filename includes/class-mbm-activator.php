157f08819c299525dc7bdf7a97796cb2bfb5b3fd
Matthew Boyles <matthew@embeemedialab.com>
2026-04-06 20:45:47 -0400

    added write defaults to class activator

--- includes/class-mbm-activator.php
+++ includes/class-mbm-activator.php
@@ -7,9 +7,14 @@ if ( ! defined( 'ABSPATH' ) ) {
 class MBM_Activator {
 
 	public static function activate(): void {
-		// Set default options, create tables, flush rewrite rules, etc.
 		if ( ! get_option( 'mbm_wp_admin_toolkit_version' ) ) {
 			update_option( 'mbm_wp_admin_toolkit_version', MBM_WP_ADMIN_TOOLKIT_VERSION );
+
+			// Write all defaults to DB on first activation
+			foreach ( MBM_Settings::$defaults as $key => $value ) {
+				// add_option won't overwrite if it already exists
+				add_option( $key, $value );
+			}
 		}
 
 		flush_rewrite_rules();
